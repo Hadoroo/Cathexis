7b5dbe022985a60f0dd6a3cb502a51e5a9ce3247
Hadoroo <128714895+Hadoroo@users.noreply.github.com>
2025-05-05 21:42:41 +0700

    bug fixes

--- Assets/DialogueManager.cs
+++ Assets/DialogueManager.cs
@@ -1,16 +1,81 @@
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("UI References")]
+    public GameObject dialoguePanel;
+    public TextMeshProUGUI speakerText;
+    public TextMeshProUGUI dialogueText;
+    public Button continueButton;
+    
+    private NPCDialogue currentNPC;
+    private int currentDialogueSet = 0;
+    private int currentLine = 0;
+    
     void Start()
     {
-        
+        dialoguePanel.SetActive(false);
+        continueButton.onClick.AddListener(ContinueDialogue);
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    public void StartDialogue(NPCDialogue npc)
     {
+        currentNPC = npc;
+        currentDialogueSet = 0;
+        currentLine = 0;
         
+        if (currentNPC.dialogueDictionary.Count == 0)
+        {
+            Debug.LogWarning($"No dialogues found for NPC {currentNPC.npcID}");
+            return;
+        }
+        
+        dialoguePanel.SetActive(true);
+        DisplayCurrentLine();
+    }
+    
+    private void DisplayCurrentLine()
+    {
+        var currentDialogue = currentNPC.dialogueDictionary[currentDialogueSet];
+        var line = currentDialogue.dialogues[currentLine];
+        
+        speakerText.text = line.speaker;
+        dialogueText.text = line.text;
+    }
+    
+    public void ContinueDialogue()
+    {
+        var currentDialogue = currentNPC.dialogueDictionary[currentDialogueSet];
+        
+        currentLine++;
+        if (currentLine >= currentDialogue.dialogues.Count)
+        {
+            // End of this dialogue set
+            dialoguePanel.SetActive(false);
+            
+            // If you want to cycle through multiple dialogue sets:
+            // currentDialogueSet = (currentDialogueSet + 1) % currentNPC.dialogueDictionary.Count;
+            return;
+        }
+        
+        DisplayCurrentLine();
+    }
+    
+    // Call this from your NPC's OnPlayerInteract event
+    public void HandleNPCInteraction(NPCDialogue npc)
+    {
+        if (currentNPC == null || currentNPC != npc)
+        {
+            // New NPC interaction
+            StartDialogue(npc);
+        }
+        else
+        {
+            // Continuing with same NPC
+            ContinueDialogue();
+        }
     }
 }
--- Assets/Scripts/NPCDialogue.cs
+++ Assets/Scripts/NPCDialogue.cs
@@ -1,15 +1,19 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections.Generic;
+using UnityEngine.Events;
+
 public class NPCDialogue : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     private Transform player;
     private SpriteRenderer speechBubble;
-
     private DialogueDatabase dialogueDatabase;
 
-    public Dialogue dialogueSO;
+    private bool canInteract = false;
+
+    [Header("Dialogue Settings")]
+    public string npcID; // Unique identifier for this NPC
+    public UnityEvent<NPCDialogue> OnPlayerInteract; // Passes this NPC reference
 
     public List<(int dialogueNum, List<(string speaker, string text)> dialogues)> dialogueDictionary = new List<(int, List<(string, string)>)>();
 
@@ -18,11 +22,26 @@ public class NPCDialogue : MonoBehaviour
         speechBubble = GetComponent<SpriteRenderer>();
         speechBubble.enabled = false;
 
+        // If no NPC ID specified, use parent name
+        if (string.IsNullOrEmpty(npcID))
+        {
+            npcID = transform.parent.name;
+        }
+
+        LoadDialogues();
+    }
+
+    void Update()
+    {
+        if (canInteract && Input.GetKeyDown(KeyCode.E))
+        {
+            OnPlayerInteract?.Invoke(this);
+        }
+    }
+
+    private void LoadDialogues()
+    {
         dialogueDatabase = ReadDialogueData.LoadData();
-
-        string soPath = "ScriptableObjects/" + transform.parent.name;
-        dialogueSO = Resources.Load<Dialogue>(soPath);
-
         int dialogueNum = 0;
 
         foreach (var location in dialogueDatabase.Location)
@@ -31,7 +50,7 @@ public class NPCDialogue : MonoBehaviour
             {
                 foreach (var content in location.Content)
                 {
-                    if (content.Initiator == transform.parent.name)
+                    if (content.Initiator == npcID) // Use npcID instead of transform.parent.name
                     {
                         var dialogueList = new List<(string speaker, string text)>();
 
@@ -47,41 +66,32 @@ public class NPCDialogue : MonoBehaviour
             }
         }
 
-        string queue = "0";
-
-        foreach (var dialoguePair in dialogueDictionary)
-        {
-            foreach (var line in dialoguePair.dialogues)  // dialogues is the List<(string, string)>
-            {
-                Debug.Log($"{line.speaker}: {line.text}");  // Access tuple elements correctly
-            }   
-
-        }
-
-
-
+        Debug.Log($"Loaded {dialogueDictionary.Count} dialogue sets for NPC {npcID}");
     }
 
     private void OnTriggerStay2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.CompareTag("Player"))
         {
             speechBubble.enabled = true;
+            player = other.transform;
 
-            player = other.gameObject.GetComponent<Transform>();
-
+            // Face player
             float direction = player.position.x > transform.position.x ? 1 : -1;
             transform.parent.localScale = new Vector3(direction,
                                                    transform.parent.localScale.y,
                                                    transform.parent.localScale.z);
+
+            canInteract = true;
         }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.CompareTag("Player"))
         {
             speechBubble.enabled = false;
+            canInteract = false;
         }
     }
 }
